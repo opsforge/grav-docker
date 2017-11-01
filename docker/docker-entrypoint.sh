e3709313ea96c841ad68b1634720d3bd3982b9e9
George Svachulay <opsforge-n7>
2017-11-01 22:27:19 +0000

    Alpha build

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! [ -e index.php -a -e bin/grav ]; then
+  echo "Required files not found in $PWD - Copying from ${SOURCE}..."
+  cp -r "$SOURCE"/. $PWD
+  chown -R www-data:www-data $PWD
+  echo "Done."
+fi
